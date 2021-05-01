3cbdfba2dd6e2a0a7138dda3040951c4fa268dd2
Sp00Rthi <spoorthyts@gmail.com>
2021-05-01 15:56:15 +0530

    git practice spoo

--- Test1/src/main/java/com/test/Test1/App.java
+++ Test1/src/main/java/com/test/Test1/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Practice git" );
+	int a=100;
+	System.out.println("value " +a);
     }
 }
